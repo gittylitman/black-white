31cdb095fe4f205fda873b5f62ce2700593a5ef1
M. Kozlik <malkak@skyvar.co.il>
2025-07-14 12:39:50 +0300

    :package: try build exe

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
+import os
 from dotenv import load_dotenv
 from process.login import setup_ui
 from config.const import TEXTS
 from modules.set_system_variable import get_env_instance
 import flet as ft
-import os
 
 load_dotenv()
 
 gcloud_path = get_env_instance().GCLOUD_PATH
 
-gcloud_path = gcloud_path
 os.environ["PATH"] = gcloud_path + os.pathsep + os.environ["PATH"]
 
 
