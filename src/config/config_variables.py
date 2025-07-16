1b232e4110bc858d244114a75ebe61d410f9c5c5
M. Kozlik <malkak@skyvar.co.il>
2025-07-16 09:23:41 +0300

    :recycle: Remove ENVIRONMENT TYPE from const

--- src/config/config_variables.py
+++ src/config/config_variables.py
@@ -1,3 +1,2 @@
-ENVIRONMENT_TYPE = "DEV"
 ACTION_TYPE = "Upload"
 GCLOUD_PATH = r"C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin"
--- src/modules/set_system_variable.py
+++ src/modules/set_system_variable.py
@@ -1,10 +1,9 @@
-from config.config_variables import ACTION_TYPE, ENVIRONMENT_TYPE, GCLOUD_PATH
+from config.config_variables import ACTION_TYPE, GCLOUD_PATH
 
 
 class _SetupEnv:
     def init_settings(self):
         try:
-            self.ENVIRONMENT_TYPE: str = ENVIRONMENT_TYPE
             self.ACTION_TYPE: str = ACTION_TYPE
             self.GCLOUD_PATH: str = GCLOUD_PATH
 
