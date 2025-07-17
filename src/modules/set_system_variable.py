5f52555ea65cf2cc99fd6912c436e52e92124baf
GShapira <gshapira20@dev.sky320.com>
2025-07-17 14:18:08 +0300

    WIP

--- src/modules/set_system_variable.py
+++ src/modules/set_system_variable.py
@@ -1,20 +1,42 @@
-from config.config_variables import ACTION_TYPE, GCLOUD_PATH
+# from config.config_variables import ACTION_TYPE, GCLOUD_PATH
 
 
+# class _SetupEnv:
+#     def init_settings(self):
+#         try:
+#             self.ACTION_TYPE: str = ACTION_TYPE
+#             self.GCLOUD_PATH: str = GCLOUD_PATH
+
+#         except KeyError as e:
+#             raise ValueError(f"Missing key in env file: {e}")
+#         return self
+
+
+# env = None
+
+
+# def get_env_instance() -> _SetupEnv:
+#     try:
+#         global env
+#         if env is None:
+#             env = _SetupEnv().init_settings()
+#         return env
+#     except Exception as error:
+#         raise Exception(str(error))
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class _SetupEnv:
     def init_settings(self):
         try:
-            self.ACTION_TYPE: str = ACTION_TYPE
-            self.GCLOUD_PATH: str = GCLOUD_PATH
+            self.ACTION_TYPE: str = os.environ["ACTION_TYPE"]
+            self.GCLOUD_PATH: str = os.environ["GCLOUD_PATH"]
 
         except KeyError as e:
             raise ValueError(f"Missing key in env file: {e}")
         return self
-
-
 env = None
-
-
 def get_env_instance() -> _SetupEnv:
     try:
         global env
