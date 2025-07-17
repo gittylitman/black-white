9fd39dceabc8349aa39b7052fbbf5ee1a5f80506
M. Kozlik <malkak@skyvar.co.il>
2025-07-17 13:18:11 +0300

    :package: Write to the file

--- src/modules/set_system_variable.py
+++ src/modules/set_system_variable.py
@@ -1,10 +1,10 @@
-import os
+from config.config_variables import ACTION_TYPE, GCLOUD_PATH
 
 class _SetupEnv:
     def init_settings(self):
         try:
-            self.ACTION_TYPE: str = os.environ["ACTION_TYPE"]
-            self.GCLOUD_PATH: str = os.environ["GCLOUD_PATH"]
+            self.ACTION_TYPE: str = ACTION_TYPE
+            self.GCLOUD_PATH: str = GCLOUD_PATH
 
         except KeyError as e:
             raise ValueError(f"Missing key in env: {e}")
